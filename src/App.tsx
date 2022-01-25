9e5b79512bb6fba4b94bfb2927ac94754ab27347
Ian Boyle <irboyle21@gmail.com>
2022-01-25 13:44:05 -0500

    adds boilerplate to APp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <div className="header">
+        <input type="text" placeholder="test" />
+        <input type="number" placeholder="Deadline (days)" />
+        <button>Add Task</button>
+      </div>
+      <div className="todoList">
+      </div>
     </div>
   );
 }
