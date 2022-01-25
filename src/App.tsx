3043a0b25f7e9c66d002f15f3771ed8de2f64f99
Ian Boyle <irboyle21@gmail.com>
2022-01-25 13:55:25 -0500

    adds handleChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, {FC} from 'react';
+import { hostname } from 'os';
+import React, {FC, ChangeEvent, useState} from 'react';
 
 import './App.css';
 
 const App = () => {
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodoList] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value) 
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  }
   return (
     <div className="App">
       <div className="header">
-        <input type="text" placeholder="test" />
-        <input type="number" placeholder="Deadline (days)" />
+        <div className="inputContainer"> 
+        <input type="text" placeholder="test" name="task" onChange={handleChange}/>
+        <input type="number" name="deadline" placeholder="Deadline (days)" onChange={handleChange}/>
+        </div>    
         <button>Add Task</button>
       </div>
       <div className="todoList">
